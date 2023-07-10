d68b2b4be6952e650bf4da971c19ed9ab69a9e5d
saniby <mbusani66@gmail.com>
2023-07-10 23:55:27 +0200

    u

--- myapp/src/comps/test.js
+++ myapp/src/comps/test.js
@@ -2,7 +2,7 @@ function Test(){
     return(
         <>
         <p>Testing component</p>
-        <p><strong>Hello error fix</strong></p>
+        <p><strong>Hello error fixes</strong></p>
         <h2>Hello people my people</h2>
         <p>South africa</p>
         </>
